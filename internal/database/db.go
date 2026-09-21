6abd6a99ca9b860eaaa735c44910db6a44d9e2a7
u-topía development <utopia.dev.contacto@gmail.com>
2026-09-21 10:40:20 -0600

    feat: add PostgreSQL connection pool

---
+++ internal/database/db.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, databaseURL string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("parse database config: %w", err)
+	}
+
+	config.MaxConns = 10
+	config.MinConns = 1
+	config.MaxConnLifetime = 30 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("create database pool: %w", err)
+	}
+
+	return pool, nil
+}
